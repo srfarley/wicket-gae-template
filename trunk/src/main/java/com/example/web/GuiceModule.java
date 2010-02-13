5650ce8c40c97b6e7149f4bb415b46e1c1484e77
srfarley <srfarley@56dc6e62-11eb-11df-92fd-07e443e6c06b>
2010-02-13 22:53:39 +0000

    Deleted old classes.

--- trunk/src/main/java/com/example/web/GuiceModule.java
+++ trunk/src/main/java/com/example/web/GuiceModule.java
@@ -1,13 +1,9 @@
 package com.example.web;
 
 import com.example.Greetings;
-import com.example.Repository;
 import com.example.jdo.PersistenceManagerFilter;
-import com.example.model.Message;
 import com.example.service.GreetingRepository;
-import com.example.service.MessageRepository;
 import com.google.inject.AbstractModule;
-import com.google.inject.TypeLiteral;
 
 /**
  * This Guice module sets up the bindings used in this Wicket application, including the
@@ -22,7 +18,6 @@ public class GuiceModule extends AbstractModule
         install(new PersistenceManagerFilter.GuiceModule());
 
         // business object bindings go here
-        bind(new TypeLiteral<Repository<Message>>() { }).to(MessageRepository.class);
         bind(Greetings.class).to(GreetingRepository.class);
     }
 }
